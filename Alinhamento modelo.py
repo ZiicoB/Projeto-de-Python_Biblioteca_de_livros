2831f8ae9c48997f134f61e6fd98337b60d11b76
ZiicoB <132504265+ZiicoB@users.noreply.github.com>
2023-10-30 09:54:36 -0300

    Atualizando modelo de alinhamento
    Tentei colocar COR nos favoritos, porem estava dando erro no alinhamento.
    corrigi fazendo uma gambiarra, colocando um alinhamento dentro da formatação com alinhamento, mas pegou

--- Alinhamento modelo.py
+++ Alinhamento modelo.py
@@ -1,14 +1,35 @@
 import os
 os.system("cls")
 
+amarelo = "\033[33m"
+azul = "\033[34m"
+azulClaro = "\033[94m"
+fimCor = "\033[m"
+
 cabecalhos = ["Título do Livro", "Favorito", "Autor", "Categoria", "Valor"]
-livro1 = ["Círculo de Fogo", "★", "Nora Sobertis", "Ficção", "253.25"]
+livro1 = ["Círculo de Fogo", "★", "Nora Robertis", "Ficção", "253.25"]
 livro2 = ["Senhos dos Anéis: A Nova Era", "-", "André Boarte Silvaas", "Comédia Romântica", "73.53"]
 
+linha = "| {:<30} | {:^9} | {:^22} | {:^20} | {:^10} "
 # Imprimir encabezados de la tabla
-print("{:<30} {:<14} {:<23} {:<22} {:^10}".format(cabecalhos[0], cabecalhos[1], cabecalhos[2], cabecalhos[3], cabecalhos[4]))
-print("=-=" * 35)
+cabecalho = linha.format(cabecalhos[0], cabecalhos[1], cabecalhos[2], cabecalhos[3], cabecalhos[4])
 
 # Imprimir información de los libros
-print("{:<33} {:<11} {:<23} {:<24} R${:^8}".format(livro1[0], livro1[1], livro1[2], livro1[3], livro1[4]))
-print("{:<33} {:<11} {:<23} {:<24} R${:^8}".format(livro2[0], livro2[1], livro2[2], livro2[3], livro2[4]))
+#linha1 = linha.format(livro1[0], livro1[1], livro1[2], livro1[3], livro1[4])
+#linha2 = linha.format(livro2[0], livro2[1], livro2[2], livro2[3], livro2[4])
+
+favorito = amarelo + livro1[1] + fimCor
+print(cabecalho)
+print("=" * len(cabecalho))
+if livro1[1] == "★":
+    print(linha.format(livro1[0], livro1[1], livro1[2], livro1[3], livro1[4]))
+else:
+    print(linha.format(livro1[0], livro1[1], livro1[2], livro1[3], livro1[4]))
+
+if livro2[1] == "★":
+    print(linha.format(livro2[0], amarelo + livro2[1] + fimCor, livro2[2], livro2[3], livro2[4]))
+else:
+    print(linha.format(livro2[0], livro2[1], livro2[2], livro2[3], livro2[4]))
+
+#print(linha2)
+print("-" * len(cabecalho))
